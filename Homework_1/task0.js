19177c5772d5e011b8e366efc66c1a53c065dbcb
mmveres <mmveres@gmail.com>
2023-03-15 14:34:52 +0200

    hw1 task3

--- Homework_1/task0.js
+++ Homework_1/task0.js
@@ -1,7 +1,12 @@
+// Write an `add()` function that will take any number of parameters in the next manner:
+// ```js
+//    console.log(add(2)(5)(7)(1)(6)(5)(10)()); // 36
+//    ```
+
 function add(num) {
-  let sum = num;    
+  let sum = num;
   return addNext;
-  
+
   function addNext(num) {
     if (num === undefined) {
       return sum;
